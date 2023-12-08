b1b75de3c7b79f0a63c2d05e1ac467104eb0f3ed
Andisurya01 <andi.suryapriyadi@gmail.com>
2023-12-08 15:54:58 +0700

    -

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -16,16 +16,8 @@ const Galerry = () => {
                 <motion.div
                     variants={galleryContainer}
                     whileHover={"hover"}
-                    className="w-full drop-shadow-lg bg-auto h-80 rounded-2xl col-span-2 overflow-hidden"
+                    className="w-full drop-shadow-lg bg-auto h-80 bg-[url('/gallery/matapink.png')] rounded-2xl col-span-2 overflow-hidden"
                 >
-                    <motion.div
-                        initial={{
-                            y: -650
-                        }}
-                    >
-                        <img
-                            src="gallery/matapink.png" className="" />
-                    </motion.div>
                 </motion.div>
                 <motion.div
                     variants={galleryContainer}
